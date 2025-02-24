2a42d1be83925e09b7598603fb0889def5a14a32
Ryan Mcmanus <35876565+r-j-mcmanus@users.noreply.github.com>
2025-02-24 15:40:13 +0000

    removed unused modules

--- script.py
+++ script.py
@@ -1,7 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from gensim.models import KeyedVectors
 from sklearn.decomposition import PCA
 import gensim.downloader as api
 
